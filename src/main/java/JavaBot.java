81d9e66747f0fd4bae4fe345e70ef4037d0dc444
kevin <94969451+kevintanjc@users.noreply.github.com>
2025-08-20 16:20:12 +0800

    Echo

--- src/main/java/JavaBot.java
+++ src/main/java/JavaBot.java
@@ -1,14 +1,30 @@
+import java.util.Scanner;
+import static resources.util.BotConstants.*;
+
 public class JavaBot {
     public static void main(String[] args) {
-        String line = "_________________________________________\n";
         System.out.println(
-                line
+                LINE_SEPARATOR + "\n"
                 + "Hello! I'm JavaBot\n"
                 + "What can I do for you?\n"
         );
 
+        executeService();
+
         System.out.println(
                 "See you next time!\n"
-                + line);
+                + LINE_SEPARATOR + "\n");
+    }
+
+    public static void executeService() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals(EXIT_COMMAND)) {
+                break;
+            } else {
+                System.out.println(input);
+            }
+        }
     }
 }
---
+++ src/main/java/resources/util/BotConstants.java
@@ -0,0 +1,9 @@
+package resources.util;
+
+public abstract class BotConstants {
+
+    public static final String LINE_SEPARATOR = "_________________________________________";
+
+    public static final String EXIT_COMMAND = "bye";
+
+}
