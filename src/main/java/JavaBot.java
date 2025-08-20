3819ed22924e97d0a297f8b34c089feaa082285d
kevin <94969451+kevintanjc@users.noreply.github.com>
2025-08-20 11:44:38 +0800

    Rename, Greet, Exit

---
+++ src/main/java/JavaBot.java
@@ -0,0 +1,14 @@
+public class JavaBot {
+    public static void main(String[] args) {
+        String line = "_________________________________________\n";
+        System.out.println(
+                line
+                + "Hello! I'm JavaBot\n"
+                + "What can I do for you?\n"
+        );
+
+        System.out.println(
+                "See you next time!\n"
+                + line);
+    }
+}
