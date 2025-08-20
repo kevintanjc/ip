481848c5687469d1fbaef702047659b7778d017b
kevin <94969451+kevintanjc@users.noreply.github.com>
2025-08-20 18:05:33 +0800

    add checklist feature
    - created a task class to implement checklist feature
    - allows marking/unmarking of tasks

--- src/main/java/resources/util/BotConstants.java
+++ src/main/java/resources/util/BotConstants.java
@@ -10,4 +10,12 @@ public abstract class BotConstants {
 
     public static final String LIST_COMMAND = "list";
 
+    public static final String MARK_COMMAND = "mark";
+
+    public static final String UNMARK_COMMAND = "unmark";
+
+    public static final String COMPLETED_SYMBOL = "[X]";
+
+    public static final String INCOMPLETE_SYMBOL = "[ ]";
+
 }
--- src/main/java/resources/util/BotService.java
+++ src/main/java/resources/util/BotService.java
@@ -9,24 +9,36 @@ import static resources.util.BotConstants.*;
 public class BotService {
 
     private Scanner scanner;
-    private List<String> userInputs;
+    private List<Task> checklist;
 
     public void executeService() {
         scanner = new Scanner(System.in);
-        userInputs = new ArrayList<>();
+        checklist = new ArrayList<>();
 
         while (true) {
             String input = scanner.nextLine();
             if (input.equals(EXIT_COMMAND)) {
                 break;
             } else if (input.equals(LIST_COMMAND)) {
-                System.out.println("Certainly! Here are your inputs thusfar:\n");
-                for (int i = 0; i < userInputs.size(); i++) {
-                    System.out.println(INDENT + (i + 1) + ". " + userInputs.get(i));
+                listTasks();
+            } else if (input.length() >= 6 && input.substring(0, 4).equals(MARK_COMMAND)) {
+                Integer index = Integer.parseInt(input.substring(5)) - 1;
+                if (index < input.length()) {
+                    markTask(index);
+                } else {
+                    System.out.println(INDENT + "Please provide a valid task number to mark.");
+                }
+            } else if (input.length() >= 8 && input.substring(0, 6).equals(UNMARK_COMMAND)) {
+                Integer index = Integer.parseInt(input.substring(7)) - 1;
+                if (index < input.length()) {
+                    unmarkTask(index);
+                } else {
+                    System.out.println(INDENT + "Please provide a valid task number to unmark.");
                 }
             } else {
-                userInputs.add(input);
-                System.out.println(INDENT + "Added: " + input);
+                Task tasking = new Task(input);
+                checklist.add(tasking);
+                System.out.println(INDENT + "Added: " + tasking.getDescription());
             }
         }
 
@@ -34,17 +46,47 @@ public class BotService {
         endBotService();
     }
 
+    private void listTasks() {
+        System.out.println("Certainly! Here are your inputs thusfar:\n");
+        for (int i = 0; i < checklist.size(); i++) {
+            if (checklist.get(i).isCompleted()) {
+                System.out.println(INDENT + (i + 1) + ". [X] " + checklist.get(i).getDescription());
+            } else {
+                System.out.println(INDENT + (i + 1) + ". [ ] " + checklist.get(i).getDescription());
+            }
+        }
+    }
+
+    private void markTask(int index) {
+        Task task = checklist.get(index);
+        if (task.isCompleted()) {
+            System.out.println(INDENT + "The following task has already been marked as done: " + task.getDescription());
+        } else {
+            task.setCompleted();
+            System.out.println(INDENT + "Well done! I'll mark it as done for you.\n" + INDENT
+                    + COMPLETED_SYMBOL + " " + task.getDescription());
+        }
+    }
+
+    private void unmarkTask(int index) {
+        Task task = checklist.get(index);
+        if (!task.isCompleted()) {
+            System.out.println(INDENT + "The following task has already been marked as not done: " + task.getDescription());
+        } else {
+            task.setCompleted();
+            System.out.println(INDENT + "Okay! I'll mark it as not done for you.\n" + INDENT + INCOMPLETE_SYMBOL
+                    + " " + task.getDescription());
+        }
+    }
+
+
+
     private void startBotService() {
-        System.out.println(LINE_SEPARATOR + "\n"
-                + "Hello! I'm JavaBot\n"
-                + "What can I do for you?\n"
-        );
+        System.out.println(LINE_SEPARATOR + "\n" + "Hello! I'm JavaBot\n" + "What can I do for you?\n");
     }
 
     private void endBotService() {
-        System.out.println("See you next time!\n"
-                + LINE_SEPARATOR + "\n"
-        );
+        System.out.println("See you next time!\n" + LINE_SEPARATOR + "\n");
     }
 
     public BotService() {
