d4fefdc2d3a82797cc36f9d696bf20f656a42808
kevin <94969451+kevintanjc@users.noreply.github.com>
2025-08-26 15:05:36 +0800

    add FILE_PATH to list of constants

--- src/main/java/resources/util/constants/BotConstants.java
+++ src/main/java/resources/util/constants/BotConstants.java
@@ -30,6 +30,8 @@ public abstract class BotConstants {
 
     public static final String NO_DATE_GIVEN = "I DUNNO >.<";
 
+    public static final String FILE_PATH = "src/data/checklist.txt";
+
     public enum Symbol {
         COMPLETED("[X]"),
         INCOMPLETE("[ ]");
