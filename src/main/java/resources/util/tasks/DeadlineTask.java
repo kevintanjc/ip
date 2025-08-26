f93a5897d8246e6feb7407840fbc4fdb33d3c377
kevin <94969451+kevintanjc@users.noreply.github.com>
2025-08-26 18:44:13 +0800

    integrate date format changes to Event and Deadline Tasks

--- src/main/java/resources/util/tasks/DeadlineTask.java
+++ src/main/java/resources/util/tasks/DeadlineTask.java
@@ -1,20 +1,30 @@
 package resources.util.tasks;
 
+import resources.util.parsers.DateTimeUtil;
+
+import java.time.LocalDateTime;
+
+import static java.util.Objects.isNull;
 import static resources.util.constants.BotConstants.DEADLINE_TASK_TYPE;
+import static resources.util.constants.BotConstants.NO_DATE_GIVEN;
 
 public class DeadlineTask extends Task {
 
-    private String endDate;
+    private LocalDateTime endDate;
 
     private static final String TASK_TYPE = DEADLINE_TASK_TYPE;
 
-    public DeadlineTask(String description, String endDate) {
+    public DeadlineTask(String description, LocalDateTime endDate) {
         super(description);
         this.endDate = endDate;
     }
 
     @Override
     public String toString() {
-        return String.format("%s%s (by: %s)", TASK_TYPE, super.toString(), endDate);
+        String formattedEndDate = isNull(endDate)
+                ? NO_DATE_GIVEN
+                : DateTimeUtil.convertLocalDateToFormattedString(endDate);
+
+        return String.format("%s%s (by: %s)", TASK_TYPE, super.toString(), formattedEndDate);
     }
 }
--- src/main/java/resources/util/tasks/EventTask.java
+++ src/main/java/resources/util/tasks/EventTask.java
@@ -1,16 +1,21 @@
 package resources.util.tasks;
 
+import java.time.LocalDateTime;
+import resources.util.parsers.DateTimeUtil;
+
+import static java.util.Objects.isNull;
 import static resources.util.constants.BotConstants.EVENT_TASK_TYPE;
+import static resources.util.constants.BotConstants.NO_DATE_GIVEN;
 
 public class EventTask extends Task {
 
-    private String startDate;
+    private LocalDateTime startDate;
 
-    private String endDate;
+    private LocalDateTime endDate;
 
     private static final String TASK_TYPE = EVENT_TASK_TYPE;
 
-    public EventTask(String description, String startDate, String endDate) {
+    public EventTask(String description, LocalDateTime startDate, LocalDateTime endDate) {
         super(description);
         this.startDate = startDate;
         this.endDate = endDate;
@@ -18,6 +23,13 @@ public class EventTask extends Task {
 
     @Override
     public String toString() {
-        return String.format("%s%s (from: %s to: %s)", TASK_TYPE, super.toString(), startDate, endDate);
+        String formattedStartDate = isNull(startDate)
+                ? NO_DATE_GIVEN
+                : DateTimeUtil.convertLocalDateToFormattedString(startDate);
+        String formattedEndDate = isNull(endDate)
+                ? NO_DATE_GIVEN
+                : DateTimeUtil.convertLocalDateToFormattedString(endDate);
+
+        return String.format("%s%s (from: %s to: %s)", TASK_TYPE, super.toString(), formattedStartDate, formattedEndDate);
     }
 }
